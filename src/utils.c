85e8b2859c5e08df77fdacb0e244165daac0e12e
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-10 14:34:35 -0500

    More renaming

--- src/utils.c
+++ src/utils.c
@@ -3,20 +3,20 @@
  *
  * Copyright (C) 2013 - 2018, Max Lv <max.c.lv@gmail.com>
  *
- * This file is part of the shadowsocks-libev.
+ * This file is part of the darksocks.
  *
- * shadowsocks-libev is free software; you can redistribute it and/or modify
+ * darksocks is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * shadowsocks-libev is distributed in the hope that it will be useful,
+ * darksocks is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with shadowsocks-libev; see the file COPYING. If not, see
+ * along with darksocks; see the file COPYING. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
@@ -264,7 +264,7 @@ void
 usage()
 {
     printf("\n");
-    printf("shadowsocks-libev %s\n\n", VERSION);
+    printf("darksocks %s\n\n", VERSION);
     printf(
         "  maintained by Max Lv <max.c.lv@gmail.com> and Linus Yang <laokongzi@gmail.com>\n\n");
     printf("  usage:\n\n");
